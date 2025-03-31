e2a07103c5a5e24f89259cf4cc8c313ec7becfde
omri_arg <r3sided@gmail.com>
2025-03-31 17:53:14 +0300

    fix

--- code/backend/naturalanomaly/urls.py
+++ code/backend/naturalanomaly/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for naturalAnomaly project.
+URL configuration for naturalanomaly project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
--- code/backend/naturalanomaly/wsgi.py
+++ code/backend/naturalanomaly/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for naturalAnomaly project.
+WSGI config for naturalanomaly project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'naturalAnomaly.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'naturalanomaly.settings')
 
 application = get_wsgi_application()
