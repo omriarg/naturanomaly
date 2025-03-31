05a469064313e9d0efd79fb955dc5cbe17f108ac
omri_arg <r3sided@gmail.com>
2025-03-31 19:02:58 +0300

    added inital ollama endpoint

---
+++ code/backend/naturalanomaly/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+import json
+from ollama import chat
+from ollama import ChatResponse
+
+def queryOlama(query):
+    body = query.body
+    response: ChatResponse = chat(model='llama3.2', messages=[
+        {
+            'role': 'user',
+            'content': query.query,
+        },
+    ])
+    print(response['message']['content'])
+    # or access fields directly from the response object
+    print(response.message.content)
+    return HttpResponse(response.message.content)
